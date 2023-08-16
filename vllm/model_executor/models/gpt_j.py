8371810b47bce6601fbaab1a2f7e3a8d1b1408a1
Carro <robertmyers@hey.com>
2023-08-16 15:00:24 -0500

    a

--- vllm/model_executor/models/gpt_j.py
+++ vllm/model_executor/models/gpt_j.py
@@ -283,11 +283,30 @@ class GPTJForSequenceClassification(nn.Module):
             cache_events
         )
         
-        rewards = self.score(hidden_states).squeeze(-1)
-        ends = torch.argmax((input_ids == 50257).type(torch.float32), dim=1).view(-1, 1)
+        logits = self.score(hidden_states)
 
-        rewards = torch.gather(rewards, 1, ends)
-        outputs = {'rewards': rewards}
+        # Extracting the logits from the last token in each sequence
+        if self.config.pad_token_id is None:
+            sequence_lengths = -1
+        else:
+            sequence_lengths = (torch.ne(input_ids, self.config.pad_token_id).sum(-1) - 1)
+
+        pooled_logits = logits[torch.arange(input_ids.shape[0]), sequence_lengths]
+
+        outputs = {"logits": pooled_logits}
+
+        # Add a loss if labels are provided
+        if labels is not None:
+            if self.num_labels == 1:
+                # Regression task
+                loss_fct = nn.MSELoss()
+                loss = loss_fct(pooled_logits.view(-1), labels.view(-1))
+            else:
+                # Classification task
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(pooled_logits.view(-1, self.num_labels), labels.view(-1))
+            outputs["loss"] = loss
+
         return outputs
 
     _column_parallel_weights = [
